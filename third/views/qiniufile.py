0d91c5b0a6d73934868f9f0dc176493152269f78
junke <wjk1397@sina.com>
2022-01-03 20:50:17 +0800

    更换soffice转换文档格式

--- third/views/qiniufile.py
+++ third/views/qiniufile.py
@@ -7,6 +7,8 @@ import random
 import string
 import threading
 import traceback
+from subprocess import TimeoutExpired
+
 import qiniu
 import requests
 
@@ -239,9 +241,11 @@ def convertAndUploadOffice(inputpath, outputpath, bucket_name, bucket_key):
         def run(self):
             try:
                 import subprocess
-                subprocess.check_output(['unoconv', '-f', 'pdf', inputpath])  #执行完毕程序才会往下进行
+                subprocess.check_output(['python3', '/var/www/DocumentConverter.py', inputpath, outputpath], timeout=60)  #执行完毕程序才会往下进行
             except ImportError:
                 logexcption(msg='引入模块失败')
+            except TimeoutExpired:
+                logexcption(msg='文件转换超时')
             except Exception:
                 logexcption(msg='文件转换失败')
             if os.path.exists(outputpath):
