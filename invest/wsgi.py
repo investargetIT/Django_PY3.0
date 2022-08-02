c62d82a2fc7ad86e17eb9ae91f06c60a56cd7ea7
junke <wjk1397@sina.com>
2022-08-02 13:16:47 +0800

    七牛文件上传标记

--- invest/wsgi.py
+++ invest/wsgi.py
@@ -9,11 +9,19 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 import django
 
 from django.core.wsgi import get_wsgi_application
 
+from invest.settings import APILOG_PATH
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "invest.settings")
 
-
+if 'uwsgi' in sys.argv:
+    if os.path.exists(APILOG_PATH['qiniumarkFilePath']):
+        os.remove(APILOG_PATH['qiniumarkFilePath'])
+    if os.path.exists(APILOG_PATH['markFilePath']):
+        os.remove(APILOG_PATH['markFilePath'])
 application = get_wsgi_application()
