4fec20afefe1635952a61d38a06a6d11ec7e6411
junke <wjk1397@sina.com>
2022-10-16 17:42:44 +0800

    文件上传接口报错

--- third/views/qiniufile.py
+++ third/views/qiniufile.py
@@ -361,7 +361,7 @@ def uploadFileToQiniu():
     elif QiNiuFileUploadRecord.objects.filter(status=2, is_deleted=False).exists():
         quesys = QiNiuFileUploadRecord.objects.filter(status=2, is_deleted=False)
         if quesys.first().starttime < (datetime.datetime.now() - datetime.timedelta(minutes=10)):
-            quesys.update(status=1, success1=False, success2=False, info1=None, info2=None, msg=None, starttime=None, endtime=None)
+            quesys.first().update(status=3, success1=False, success2=False, info1=None, info2=None, msg=None, starttime=None, endtime=None)
 
 
 # 获取七牛文件上传记录
