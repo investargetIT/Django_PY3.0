c05ae4c1176bafa9e20b72a12c9bbdc43d08f352
junke <wjk1397@sina.com>
2023-09-19 18:33:00 +0800

    企名片数据decode

--- third/views/qimingpianapi.py
+++ third/views/qimingpianapi.py
@@ -19,10 +19,14 @@ def updatePersonTag(username, orgname, tags):
             }
             url = 'https://qimingpianapi.investarget.com/Person/updatePersonTag'
             res = requests.post(url, data=data, headers={'Content-Type': 'application/x-www-form-urlencoded'}).content
-            res = json.loads(res)
+            try:
+                res = json.loads(res.decode())
+            except json.decoder.JSONDecodeError:
+                res = json.loads(res)
             if res['status'] == 0:
                 print('编辑投资人标签（投资人：%s, 机构：%s）成功' % (username, orgname))
             else:
-                raise InvestError(20071, msg='编辑投资人标签（投资人：%s, 机构：%s）失败' % (username, orgname), detail=res['message'])
+                raise InvestError(20071, msg='编辑投资人标签（投资人：%s, 机构：%s）失败' % (username, orgname),
+                                  detail=res['message'])
     except Exception:
         logexcption()
