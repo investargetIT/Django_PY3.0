06aac082fa1633395d2c77e73cb9dfd2d2984480
junke <wjk1397@sina.com>
2023-11-17 16:10:06 +0800

    更新企名片用户标签逻辑修改

--- third/views/qimingpianapi.py
+++ third/views/qimingpianapi.py
@@ -11,28 +11,45 @@ from third.thirdconfig import qimingpian_open_id
 from utils.customClass import InvestError
 from utils.util import logexcption
 
+# 企名片检索用户
+def searchPerson(keywords):
+    data = {
+        'open_id': qimingpian_open_id,
+        'keywords': keywords,
+    }
+    url = 'https://qimingpianapi.investarget.com/Person/searchPerson'
+    res = requests.post(url, data=data, headers={'Content-Type': 'application/x-www-form-urlencoded'}).content
+    res = json.loads(res)
+    if res['status'] == 0:
+        if res['data']['count'] >= 1:
+            return res['data']['list']
+    else:
+        raise InvestError(20071, msg='查询投资人（%s）失败' % str(data), detail=str(res))
 
-def updatePersonTag(username, orgname, tags):
+def updatePersonTag(usermobile, tags):
     try:
-        if username and orgname and len(tags) > 0:
-            tag_list = [tag.nameC for tag in tags]
-            data = {
-                'open_id': qimingpian_open_id,
-                'name': username,
-                'agency': orgname,
-                'content': '|'.join(tag_list),
-            }
-            url = 'https://qimingpianapi.investarget.com/Person/updatePersonTag'
-            res = requests.post(url, data=data, headers={'Content-Type': 'application/x-www-form-urlencoded'}).content
-            try:
-                res = json.loads(res.decode())
-            except json.decoder.JSONDecodeError:
-                res = json.loads(res)
-            if res['status'] == 0:
-                print('编辑投资人标签（投资人：%s, 机构：%s）成功' % (username, orgname))
-            else:
-                raise InvestError(20071, msg='编辑投资人标签（投资人：%s, 机构：%s）失败' % (username, orgname),
-                                  detail=res['message'])
+        persons = searchPerson(str(usermobile))
+        if persons:
+            person = persons[0]
+            if person['agency'] and person['name'] and len(tags) > 0:
+                tag_list = [tag.nameC for tag in tags]
+                data = {
+                    'open_id': qimingpian_open_id,
+                    'name': person['name'],
+                    'agency': person['agency'],
+                    'content': '|'.join(tag_list),
+                }
+                url = 'https://qimingpianapi.investarget.com/Person/updatePersonTag'
+                res = requests.post(url, data=data, headers={'Content-Type': 'application/x-www-form-urlencoded'}).content
+                try:
+                    res = json.loads(res.decode())
+                except json.decoder.JSONDecodeError:
+                    res = json.loads(res)
+                if res['status'] == 0:
+                    print('编辑投资人标签（投资人：%s, 机构：%s）成功' % (person['name'], person['agency']))
+                else:
+                    raise InvestError(20071, msg='编辑投资人标签（投资人：%s, 机构：%s）失败' % (person['name'], person['agency']),
+                                      detail=res['message'])
     except Exception:
         logexcption()
 
